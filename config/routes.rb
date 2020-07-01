df9bfe0105e13a8990a894f4457a1382ae2672ea
s-onms <sok5lunaamspectgdmatu@outlook.jp>
2020-07-01 23:57:41 +0900

    sns認証

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'users#index'  # ログインor新規登録を選ぶページ
+  resources :users, only: :new  # 新規登録方法を選ぶページ
 end
-
-root 'users#index'  # ログインor新規登録を選ぶページ
-resources :users, only: :new  # 新規登録方法を選ぶページ
